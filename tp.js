6e2ac5cd508f34dbbf7b2e8d6ff1df1f84296917
moahmed amine mezghich <git@mezghich.com>
2022-09-20 10:27:55 +0100

    deuxième méthode d'appel de js

--- tp.js
+++ tp.js
@@ -1,5 +1,26 @@
-var languages = ["HTML", "CSS", "Javascript", "Java", "PHP"];
+/*var languages = ["HTML", "CSS", "Javascript", "Java", "PHP"];
 
 for (var i = 0; i < languages.length; i++) {
     console.log("J'aime : " + languages[i]);
+}*/
+
+
+function messageEntre() {
+    alert("Bonjour tout le monde");
+}
+
+function messageSortie() {
+    alert("Au revoir...");
+}
+
+function formulaire(){
+ var nom = prompt("Taper votre nom"); // permet de récupérer des data
+ alert("Vous êtes : "+nom);
+}
+
+function somme(){
+    var v1  =  prompt("Taper première valeur","v1");
+    var v2  =  prompt("Taper deuxième valeur","v2");
+    var res = parseFloat(v1) + parseFloat(v2);
+    alert("Le résultat de " + v1 + " + "+v2 +" = "+res);
 }
